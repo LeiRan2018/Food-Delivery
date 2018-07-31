c16a1015ecedcdc66c22c6ada537d54bf46451f8
Leiran2018 <jackran410@gmail.com>
2018-07-31 12:32:49 -0400

    updated bills and customers routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DishComponent } from './dish/dish.component';
+import { CustomerComponent } from './customer/customer.component';
+import { BillComponent } from './bill/bill.component';
 
 const routes: Routes = [
-  { path: 'menu', component: DishComponent}
+  { path: '', redirectTo: '/menu', pathMatch: 'full'},
+  { path: 'menu', component: DishComponent},
+  { path: 'customers', component: CustomerComponent},
+  { path: 'bills', component: BillComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
