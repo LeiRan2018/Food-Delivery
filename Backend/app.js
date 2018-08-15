a3ebbd8e033f5573cd17355c47c7a92cdb283c19
Lei <jackran410@gmail.com>
2018-08-15 16:43:18 -0400

    deploy app to docker and deleted some unnecessary files

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ var app = express();
 
 var mongoose = require('mongoose')
 mongoose.Promise = bluebird
-mongoose.connect('mongodb://127.0.0.1:27017/Delivery', { useMongoClient: true})
+mongoose.connect('mongodb://mongo/Delivery', { useMongoClient: true})
 .then(()=> { console.log(`Succesfully Connected to the Mongodb Database  at URL : mongodb://127.0.0.1:27017/Delivery`)})
 .catch(()=> { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/Delivery`)})
 
